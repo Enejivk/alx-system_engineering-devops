c1289b1ca9578670b0f5b4db5eb7d20b2028836f
enejivk <enejivic@gmail.com>
2024-03-22 22:13:54 +0100

    all the file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#This is a puppet file that contain I love puppet
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
