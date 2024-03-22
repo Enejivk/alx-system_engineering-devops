0016ffcf2b251ed1e52f1f41b7f61dbc1cc867db
enejivk <enejivic@gmail.com>
2024-03-22 22:22:24 +0100

    all the file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
 # Install the puppet-lint package
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+# install_flask.pp
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => '/usr/bin',
+  environment => ['LANG=en_US.UTF-8'],
+  unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
