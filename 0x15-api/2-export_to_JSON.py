1c36f7652c6f83e1a022a56660b8bf12c1f7b2ef
enejivk <enejivic@gmail.com>
2024-05-22 23:04:00 +0100

    storing the file in json formate

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
+"""Saving the file in json formate"""
 
+import json
 import requests
 import sys
 
+
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/todos'
-    id = sys.argv[1]
-    username = requests.get(url).json()
-    
-    task = requests.get()
-    
+    id = int(sys.argv[1])
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+
+    username = requests.get(user_url).json().get('username')
+    tasks = requests.get(todo_url).json()
+
+    user_task = []
+    for task in tasks:
+        if task.get('userId') == id:
+            user_task.append({
+                "task": task.get('title'),
+                "completed": task.get('completed'),
+                "username": username})
+    dictionary = {id: user_task}
+    with open('{}.json'.format(id), 'w') as file:
+        json.dump(dictionary, file)
