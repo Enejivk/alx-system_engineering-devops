be05f0ca662442145339f48a507837b8fa7fbcf7
enejivk <enejivic@gmail.com>
2024-05-23 00:33:47 +0100

    correcting error

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,7 +7,7 @@ import requests
 if __name__ == '__main__':
     user_url = 'https://jsonplaceholder.typicode.com/users'
     todo_url = 'https://jsonplaceholder.typicode.com/todos'
-    
+
     users = requests.get(user_url).json()
     todo = requests.get(todo_url).json()
     dictionary = {}
@@ -15,9 +15,10 @@ if __name__ == '__main__':
         user_task = []
         for task in todo:
             if task.get('userId') == user.get('id'):
-                    user_task.append({"task": task.get('title'),
-                                    "completed": task.get('completed'),
-                                    "username": user.get('username')})
+                user_task.append(
+                    {"task": task.get('title'),
+                     "completed": task.get('completed'),
+                     "username": user.get('username')})
         dictionary[user.get('id')] = user_task
     with open('todo_all_employees.json', 'w') as file:
         json.dump(dictionary, file)
