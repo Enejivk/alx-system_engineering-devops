b4b53eff4646bc8b88de3e7d9ae0535399c724ff
enejivk <enejivic@gmail.com>
2024-05-22 18:07:32 +0100

    ls

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Generating todo list for a certain user and """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    total_task = 0
+    completed_task = 0
+    incompletely_task = 0
+    task_title = []
+
+    id = int(sys.argv[1])
+    userId_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+
+    name = requests.get(userId_url).json().get('name')
+    task_respond = requests.get(todo_url).json()
+
+    for task in task_respond:
+        if task.get('userId') == id:
+            total_task += 1
+            if task.get('completed') == True:
+                task_title.append(task.get('title'))
+                completed_task += 1
+            else:
+                incompletely_task += 1
+
+    print(f"Employee EMPLOYEE_NAME is done with tasks({completed_task}/{total_task}):")
+    for title_task_item in task_title:
+        print('\t', title_task_item)
