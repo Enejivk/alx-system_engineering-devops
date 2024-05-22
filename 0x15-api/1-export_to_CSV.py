a9c2956b33c219709479edb114c7fb6de7514322
enejivk <enejivic@gmail.com>
2024-05-22 20:04:35 +0100

    saving file to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     id = int(sys.argv[1])
     user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
     todo_url = 'https://jsonplaceholder.typicode.com/todos'
-    
+
     username = requests.get(user_url).json().get('username')
     tasks = requests.get(todo_url).json()
     with open('2.csv', 'w') as file:
